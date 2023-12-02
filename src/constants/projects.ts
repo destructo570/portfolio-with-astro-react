adae82b0424d7cab0dfbb9f2ff1381ad2bddc5bf
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-02 20:13:33 +0530

    feat: add projects section

---
+++ src/constants/projects.ts
@@ -0,0 +1,121 @@
+export const PROJECTS = [
+  {
+    category: "Websites",
+    projects: [
+      {
+        title: "Moekana",
+        sub_title:
+          "Minimal kanban board web app made with MERN stack and jwt authentication.",
+        description:
+          "A minimal kanban board web app made with MERN stack. Manage all your projects and tasks at one place. Create, Update and Delete boards, cards and lists to manage your projects. Backend REST Api created with Node and Express with JWT authentication, server side validation and error handling.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/moe-kana",
+      },
+      {
+        title: "Webgl Experiments",
+        sub_title: "Webgl experiments in react fiber 3.",
+        description: "Webgl experiments in react fiber 3.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/webgl-experiments",
+      },
+      {
+        title: "Portfolio",
+        sub_title: "My portfolio website made with astro.",
+        description: "My portfolio website made with astro.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/portfolio-with-astro-react",
+      },
+      {
+        title: "Kanban Board",
+        sub_title:
+          "Minimal kanban board web app made with MERN stack and jwt authentication.",
+        description:
+          "A minimal kanban board web app made with MERN stack. Manage all your projects and tasks at one place. Create, Update and Delete boards, cards and lists to manage your projects. Backend REST Api created with Node and Express with JWT authentication, server side validation and error handling.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/kanban-board-mern-stack",
+      },
+      {
+        title: "Deplace Maison Ecommerce Webapp",
+        sub_title:
+          "A full stack clone of deplace maison ecoomerce website using Next JS with stripe payment integration and firebase for storing orders.",
+        description:
+          "A full stack clone of deplace maison ecoomerce website using Next JS with stripe payment integration and firebase for storing orders.",
+        teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
+        url: "https://github.com/destructo570/deplace-maison-ecommerce-webapp",
+      },
+      {
+        title: "React Countries App",
+        sub_title:
+          "See useful information related to all countries like currency, native names, border countries etc.",
+        description:
+          "See useful information related to all countries like currency, native names, border countries etc.",
+        teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
+        url: "https://github.com/destructo570/deplace-maison-ecommerce-webapp",
+      },
+    ],
+  },
+  {
+    category: "Backend",
+    projects: [
+      {
+        title: "Moe quotes api",
+        sub_title:
+          "Rest API built with node js to serve random quotes from anime.",
+        description:
+          "Rest API built with node js to serve random quotes from anime.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/moe-quotes-api",
+      },
+    ],
+  },
+  {
+    category: "Android/Mobile",
+    projects: [
+      {
+        title: "Sushi - Unofficial MAL Client",
+        sub_title:
+          "Sushi is a modern and minimal android client for MyAnimeList.",
+        description:
+          "Sushi is a modern and minimal android client for MyAnimeList.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/Sushi-Unofficial-MAL-Client",
+      },
+      {
+        title: "Mars App",
+        sub_title:
+          "App uses NASA's API to show you the latest photos taken by all the mars rovers like curiosity, perseverance etc.",
+        description:
+          "App uses NASA's API to show you the latest photos taken by all the mars rovers like curiosity, perseverance etc.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/mars-app",
+      },
+      {
+        title: "Covid Tracker",
+        sub_title:
+          "A minimally designed app to track current statistics of COVID-19 across the world.",
+        description:
+          "A minimally designed app to track current statistics of COVID-19 across the world and detailed state wise statistics of India. App also offers some useful information regarding COVID-19, along with helpline numbers.",
+        teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
+        url: "https://github.com/destructo570/CovidTracker-kotlin",
+      },
+      {
+        title: "Erza Icon Pack",
+        sub_title:
+          "I was inspired by Google's material design and wanted to use all my design skills to create something unique.",
+        description:
+          "I was inspired by Google's material design and wanted to use all my design skills to create something unique. Erza is a material design inspired icon pack for giving your android phone a fresh and unique look.",
+        teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
+        url: "https://play.google.com/store/apps/details?id=com.erza.vishal",
+      },
+      {
+        title: "Zeref Icon Pack",
+        sub_title:
+          "Another icon pack I had alot of fun working on, I wanted to create something funky and vibrant.",
+        description:
+          "Another icon pack I had alot of fun working on, I wanted to create something funky and vibrant. Zeref is a vibrant and minimalist icon pack for those who are tired of the same old material design icon packs.",
+        teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
+        url: "https://www.github.com/destructo570",
+      },
+    ],
+  },
+];
