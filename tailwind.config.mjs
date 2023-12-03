d4f605b3cc1721fa29017499767de6703cfb8ea2
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-03 12:18:53 +0530

    style: fix blog section styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("@tailwindcss/typography")],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "card-bg": "#ffffff",
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
