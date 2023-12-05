a053a5f409244a56fa431f3e673295bc3e2326ec
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-05 09:05:18 +0530

    chore: add temporary icons for project cards

--- src/components/ProjectsList/ProjectCard.jsx
+++ src/components/ProjectsList/ProjectCard.jsx
@@ -5,7 +5,7 @@ const ProjectCard = (props) => {
   return (
     <ReactCard is_hover={true} classes="min-h-[100px]" url={url} id={id}>
       <div class="flex gap-4 card-content">
-        <img src="https:/picsum.photos/48" alt="" class="p-2" />
+        <img src="/icons/layout.svg" alt="" class="p-2" />
         <div class="p-2">
           <h4>{title}</h4>
           <p className="text-xs line-clamp-2">{sub_title}</p>
