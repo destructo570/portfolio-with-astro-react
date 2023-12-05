a232845e388bd2fdade3a45f6bda95e9d575059f
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-05 23:12:24 +0530

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  site: 'https://destructo.dev',
+  integrations: [tailwind(), mdx(), react(), sitemap()]
 });
