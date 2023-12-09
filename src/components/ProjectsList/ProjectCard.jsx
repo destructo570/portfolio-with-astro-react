784071011079f462c93577d173f06b83d55e89f3
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-09 10:08:00 +0530

    style: changes to home section

--- src/components/ProjectsList/ProjectCard.jsx
+++ src/components/ProjectsList/ProjectCard.jsx
@@ -4,11 +4,11 @@ const ProjectCard = (props) => {
   const { title, sub_title, url, id } = props;
   return (
     <ReactCard is_hover={true} classes="min-h-[100px]" url={url} id={id}>
-      <div className="flex gap-4 card-content">
-        <img src="/icons/layout.svg" alt="" className="p-2" />
-        <div className="p-2">
-          <h4 className="line-clamp-1">{title}</h4>
-          <p className="text-xs line-clamp-2">{sub_title}</p>
+      <div className="flex card-content">
+        {/* <img src="/icons/flag (1).svg" alt="" className="p-2" /> */}
+        <div className="px-4 py-2">
+          <h4 className="line-clamp-1 text-lg tracking-wide">{title}</h4>
+          <p className="text-sm line-clamp-2 tracking-wider opacity-50">{sub_title}</p>
         </div>
       </div>
     </ReactCard>
