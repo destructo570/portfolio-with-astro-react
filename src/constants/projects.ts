3ad54f6107dab7f70103fb504556b7c9a4d4521c
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-09 15:23:45 +0530

    chore: update project data

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -6,24 +6,24 @@ export const PROJECTS = [
         id: "moekana_5678",
         title: "Moekana",
         sub_title:
-          "Minimal kanban board web app made with MERN stack and jwt authentication.",
+          "Open-source web app for helping people learn and practice hiragana and katakana. Made with NextJS 13 and ShadcnUI",
         description:
-          "A minimal kanban board web app made with MERN stack. Manage all your projects and tasks at one place. Create, Update and Delete boards, cards and lists to manage your projects. Backend REST Api created with Node and Express with JWT authentication, server side validation and error handling.",
+          "An open-source app built for helping people learn and practice hiragana and katakana.This was a little side project to explore everything new that NextJs 13 offers.",
         teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
         url: "https://github.com/destructo570/moe-kana",
       },
       {
         id: "webgl_experiments_1234",
-        title: "Webgl Experiments",
-        sub_title: "Webgl experiments in react fiber 3.",
-        description: "Webgl experiments in react fiber 3.",
+        title: "WebGL Experiments",
+        sub_title: "WebGL experiments in react 3 fiber.",
+        description: "WebGL experiments in react 3 fiber.",
         teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
         url: "https://github.com/destructo570/webgl-experiments",
       },
       {
         id: "portfolio_7890",
-        title: "Portfolio",
-        sub_title: "My portfolio website made with astro.",
+        title: "Portfolio Website",
+        sub_title: "Personal website/portfolio made with Astro to showcase everything about me and my projects.",
         description: "My portfolio website made with astro.",
         teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
         url: "https://github.com/destructo570/portfolio-with-astro-react",
@@ -32,7 +32,7 @@ export const PROJECTS = [
         id: "kanban_board_4567",
         title: "Kanban Board",
         sub_title:
-          "Minimal kanban board web app made with MERN stack and jwt authentication.",
+          "Minimal kanban board web app made with MERN stack and JWT authentication.",
         description:
           "A minimal kanban board web app made with MERN stack. Manage all your projects and tasks at one place. Create, Update and Delete boards, cards and lists to manage your projects. Backend REST Api created with Node and Express with JWT authentication, server side validation and error handling.",
         teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
@@ -40,11 +40,11 @@ export const PROJECTS = [
       },
       {
         id: "deplace_maison_ecommerce_webapp_2345",
-        title: "Deplace Maison Ecommerce Webapp",
+        title: "Deplace Maison E-commerce Webapp",
         sub_title:
-          "A full stack clone of deplace maison ecoomerce website using Next JS with stripe payment integration and firebase for storing orders.",
+          "A Full stack clone of deplace maison ecoomerce website using Next JS with stripe payment integration and firebase for storing orders.",
         description:
-          "A full stack clone of deplace maison ecoomerce website using Next JS with stripe payment integration and firebase for storing orders.",
+          "A Full stack clone of deplace maison ecoomerce website using Next JS with stripe payment integration and firebase for storing orders.",
         teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
         url: "https://github.com/destructo570/deplace-maison-ecommerce-webapp",
       },
@@ -56,7 +56,27 @@ export const PROJECTS = [
         description:
           "See useful information related to all countries like currency, native names, border countries etc.",
         teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
-        url: "https://github.com/destructo570/deplace-maison-ecommerce-webapp",
+        url: "https://github.com/destructo570/react-countries",
+      },
+      {
+        id: "ip_address_tracker_10",
+        title: "IP address tracker",
+        sub_title:
+          "Frontend mentor challenge. This project gives you information regarding the entered IP Address and shows the location in a leaflet map",
+        description:
+          "Frontend mentor challenge. This project gives you information regarding the entered IP Address and shows the location in a leaflet map",
+        teck_stack: ["HTML", "CSS", "JS"],
+        url: "https://github.com/destructo570/HTML-CSS-IP-Tracker-Miniproject",
+      },
+      {
+        id: "simple_calculator_10",
+        title: "Simple Calculator",
+        sub_title:
+          "Frontend mentor challenge. Simple calculator application made with vanilla js with multiple theme options.",
+        description:
+          "Frontend mentor challenge. Simple calculator application made with vanilla js with multiple theme options.",
+        teck_stack: ["HTML", "CSS", "JS"],
+        url: "https://github.com/destructo570/frontendmaster-calculator",
       },
     ],
   },
@@ -64,14 +84,39 @@ export const PROJECTS = [
     category: "Backend",
     projects: [
       {
+        id: "kanban_board_45678",
+        title: "Kanban Board REST Api",
+        sub_title:
+          "RestFul Api for my Kanban board MERN stack app. Built with NodeJs, MongoDb, Express, Mongoose, JWT",
+        description:
+          "A minimal kanban board web app made with MERN stack. Manage all your projects and tasks at one place. Create, Update and Delete boards, cards and lists to manage your projects. Backend REST Api created with Node and Express with JWT authentication, server side validation and error handling.",
+        teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
+        url: "https://github.com/destructo570/kanban-board-mern-stack",
+      },
+      {
         id: "moe_quotes_api_9876",
         title: "Moe quotes api",
         sub_title:
-          "Rest API built with node js to serve random quotes from anime.",
+          "Rest API built with Node.js to serve random quotes from anime.",
         description:
-          "Rest API built with node js to serve random quotes from anime.",
+          "Rest API built with Node.js to serve random quotes from anime.",
         teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
         url: "https://github.com/destructo570/moe-quotes-api",
+      },
+    ],
+  },
+  {
+    category: "Games",
+    projects: [
+      {
+        id: "rock_paper_scissor_45678",
+        title: "Rock, Paper, Scissors, Lizard, Spock",
+        sub_title:
+          "Frontend mentor challenge where I needed to develop the ever popular rock, paper, scissors, lizard, spock game from Big bang!",
+        description:
+          "Frontend mentor challenge where I needed to develop the ever popular rock, paper, scissors, lizard, spock game from Big bang!",
+        teck_stack: ["HTML", "CSS", "JS"],
+        url: "https://github.com/destructo570/Rock-paper-scissors-lizard-spock-JS-Mini-Project",
       },
     ],
   },
@@ -82,9 +127,9 @@ export const PROJECTS = [
         id: "sushi_unofficial_mal_client_3456",
         title: "Sushi - Unofficial MAL Client",
         sub_title:
-          "Sushi is a modern and minimal android client for MyAnimeList.",
+          "Sushi is a modern and minimal android client for MyAnimeList. Track all your anime/manga at one place.",
         description:
-          "Sushi is a modern and minimal android client for MyAnimeList.",
+          "Sushi is a modern and minimal android client for MyAnimeList. Track all your anime/manga at one place.",
         teck_stack: ["React", "NodeJs", "MongoDb", "Express"],
         url: "https://github.com/destructo570/Sushi-Unofficial-MAL-Client",
       },
@@ -112,21 +157,21 @@ export const PROJECTS = [
         id: "erza_icon_pack_7654",
         title: "Erza Icon Pack",
         sub_title:
-          "I was inspired by Google's material design and wanted to use all my design skills to create something unique.",
+          "Inspired by Google's material design but with a twist. Erza is a colorful and unique icon pack.",
         description:
-          "I was inspired by Google's material design and wanted to use all my design skills to create something unique. Erza is a material design inspired icon pack for giving your android phone a fresh and unique look.",
+          "Inspired by Google's material design but with a twist. Erza is a colorful and unique icon pack.",
         teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
-        url: "https://play.google.com/store/apps/details?id=com.erza.vishal",
+        url: "https://dribbble.com/shots/5538949-Erza-Icon-Pack",
       },
       {
         id: "zeref_icon_pack_8765",
         title: "Zeref Icon Pack",
         sub_title:
-          "Another icon pack I had alot of fun working on, I wanted to create something funky and vibrant.",
+          "Zeref is a vibrant and minimalist icon pack for those who are tired of the same old material design icon packs.",
         description:
-          "Another icon pack I had alot of fun working on, I wanted to create something funky and vibrant. Zeref is a vibrant and minimalist icon pack for those who are tired of the same old material design icon packs.",
+          "Zeref is a vibrant and minimalist icon pack for those who are tired of the same old material design icon packs.",
         teck_stack: ["NextJs", "Firebase", "Stripe", "Next Auth"],
-        url: "https://www.github.com/destructo570",
+        url: "https://dribbble.com/shots/5554697-Zeref-Icons-Android",
       },
     ],
   },
